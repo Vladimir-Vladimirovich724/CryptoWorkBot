255c395c7c8d1f839b72e7cc6b3426379be1bdb4
Vladimir-Vladimirovich724 <globuscompany@ya.ru>
2025-08-20 01:52:56 +0400

    Update app.js

--- web-app/app.js
+++ web-app/app.js
@@ -1,6 +1,8 @@
 function buyProduct(product_id) {
+    // Выводим сообщение в консоль браузера, чтобы убедиться, что функция вызывается.
+    console.log("Попытка отправить данные:", { action: "buy", product: product_id });
+
     // Отправляем данные в Telegram.
-    // data.product будет содержать 'vip' или 'booster'.
     Telegram.WebApp.sendData(JSON.stringify({
         action: "buy",
         product: product_id
